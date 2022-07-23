081a6c2e93e08b4b537b98c098657480046adbe3
Anand Raja - Mirror <meetanandraja@gmail.com>
2022-07-23 20:58:54 +0000

    refactoring

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,12 @@ export const environment = {
   dbToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjM0MjliMzA1NzU1NzZiNTkxNTEyM2E0MjlkNDE3MyIsInN1YiI6IjYyY2VkODBkZDUxOTFmMDA1M2Y4MDAxYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.D3JiI2187leQ6L8EMkgDrMegOnQuZv0GMK3PVXLgudk',
   baseUrl: 'https://api.themoviedb.org/3',
   imgBaseUrl: 'https://image.tmdb.org/t/p',
-  manPlaceholderImg: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Man_Silhouette2.jpg',
   idmbUrl: 'https://www.imdb.com/title',
   idmbUserUrl: 'https://www.imdb.com/name',
-  movieNotFoundUrl: 'https://cdn.pixabay.com/photo/2017/01/25/17/35/camera-2008489_1280.png'
+  manPlaceholderImg: 'assets/images/man-placeholder.jpg',
+  movieNotFoundUrl: 'assets/images/movie-not-found.png',
+  // manPlaceholderImg: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Man_Silhouette2.jpg',
+  // movieNotFoundUrl: 'https://cdn.pixabay.com/photo/2017/01/25/17/35/camera-2008489_1280.png',
 };
 
 /*
